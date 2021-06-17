7ce38627ab3776395ad160df0d91228a3355e123
nidhipatel1211 <nidhi121125@gmail.com>
2021-06-17 15:51:39 +0530

    Create variable.java

---
+++ NidhiPatel/src/nidhipatel/variable.java
@@ -0,0 +1,14 @@
+package nidhipatel;
+
+public class variable {
+
+	public static void main(String[] args) {
+		int a=5;
+		float f=6;
+		String s="nidhi";
+		System.out.println("value of int variable"+a);
+		System.out.println("value of float variable"+f);
+		System.out.println("value of string variable"+s);
+	}
+
+}
