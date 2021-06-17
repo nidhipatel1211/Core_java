1923527271da592ec88df746d1d5ce0c05cbd7e5
nidhipatel1211 <nidhi121125@gmail.com>
2021-06-17 17:59:07 +0530

    Create string.java

---
+++ NidhiPatel/nidhi/src/nidhi/string.java
@@ -0,0 +1,12 @@
+package nidhi;
+
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a="20";
+String b="10";
+System.out.println(a+b);
+	}
+
+}
