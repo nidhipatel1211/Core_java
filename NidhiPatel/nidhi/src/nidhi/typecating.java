9c5eba50ecb69fd1a93215fe44a3443ad385ca37
nidhipatel1211 <nidhi121125@gmail.com>
2021-06-18 17:40:04 +0530

    Create typecating.java

---
+++ NidhiPatel/nidhi/src/nidhi/typecating.java
@@ -0,0 +1,21 @@
+package nidhi;
+
+public class typecating {
+
+	public static void main(String[] args) {
+		short s=4;
+		int x=s;
+		System.out.println(x);
+		int y=7;
+		short z=(short)y;
+		System.out.println(z);
+		double d=78.77;
+		float f=(float)d;
+		System.out.println(f);
+		double dd=f;
+		System.out.println(dd);
+		long l=(long)f;
+		System.out.println(l);
+	}
+
+}
