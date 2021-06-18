16db4684a2322059d9c237d9ef7837c57bd60230
nidhipatel1211 <nidhi121125@gmail.com>
2021-06-18 18:01:25 +0530

    Create airthmetic_operator.java

---
+++ NidhiPatel/nidhi/src/nidhi/airthmetic_operator.java
@@ -0,0 +1,36 @@
+package nidhi;
+
+public class airthmetic_operator {
+
+	public static void main(String[] args) {
+		int x=50;
+		int y=50;
+		//post increment
+		System.out.println(x++);
+		System.out.println(x);
+		//post decrement
+		System.out.println(x--);
+		System.out.println(x);
+		//pre increment
+		System.out.println(++x);
+		System.out.println(x);
+		//pre decrement
+		System.out.println(--x);
+		System.out.println(x);
+		//addition
+		System.out.println(x+y);
+		//subtraction
+		System.out.println(x-y);
+		//multiplication
+		System.out.println(x*y);
+		//division
+		System.out.println(x/y);
+		//mod
+		System.out.println(x%y);
+		
+		;
+		
+
+	}
+
+}
