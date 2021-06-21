ce70e851a43ed7dad761954c9e177c79a5f55bf9
nidhipatel1211 <nidhi121125@gmail.com>
2021-06-21 16:17:10 +0530

    Create assignment_operator.java

---
+++ NidhiPatel/nidhi/src/nidhi/assignment_operator.java
@@ -0,0 +1,17 @@
+package nidhi;
+
+public class assignment_operator {
+
+	public static void main(String[] args) {
+		int x=5;
+		x=+x;
+		System.out.println(x);
+		x=-x;
+		System.out.println(x);
+		x=x*x;
+		System.out.println(x);
+		x=x/x;
+		System.out.println(x);
+	}
+
+}
