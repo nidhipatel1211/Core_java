e210a7955e5230c825fc2c9d92113348ee82a568
nidhipatel1211 <nidhi121125@gmail.com>
2021-06-21 16:12:06 +0530

    Create Ternary_operator.java

---
+++ NidhiPatel/nidhi/src/nidhi/Ternary_operator.java
@@ -0,0 +1,15 @@
+package nidhi;
+
+public class Ternary_operator {
+
+	public static void main(String[] args) {
+		int x=5,y=8;
+		boolean b;
+		b=(x>y)?true:false;
+		System.out.println(b);
+		
+		
+
+	}
+
+}
