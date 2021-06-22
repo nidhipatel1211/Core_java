3ddcc9a05af33b4c9aa1fd9eaa004b7cb9bb84d0
nidhipatel1211 <nidhi121125@gmail.com>
2021-06-22 15:51:05 +0530

    Create while_do_while_loop.java

---
+++ NidhiPatel/nidhi/src/nidhi/while_do_while_loop.java
@@ -0,0 +1,17 @@
+package nidhi;
+
+public class while_do_while_loop {
+
+	public static void main(String[] args) {
+		int a = 1;
+		while (a <=10) {
+			System.out.println(a);
+			a++;
+		}
+		int i=1;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <=10);
+	}
+}
