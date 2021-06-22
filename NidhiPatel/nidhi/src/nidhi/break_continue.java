0d8a7c8f050ae340aed2175401034819b9264ee8
nidhipatel1211 <nidhi121125@gmail.com>
2021-06-22 15:58:59 +0530

    Create break_continue.java

---
+++ NidhiPatel/nidhi/src/nidhi/break_continue.java
@@ -0,0 +1,24 @@
+package nidhi;
+
+public class break_continue {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+			if(i==5)
+			{
+				break;
+			}
+		}
+		for(int i=1;i<=10;i++)
+		{
+			if(i==5)
+			{
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
