63321538f4e9167553967d1bbd2cf5aa9e6a890f
nidhipatel1211 <nidhi121125@gmail.com>
2021-06-24 16:04:41 +0530

    Create getter_setter.java

---
+++ NidhiPatel/nidhi/src/nidhi/getter_setter.java
@@ -0,0 +1,11 @@
+package nidhi;
+
+public class getter_setter {
+
+	public static void main(String[] args) {
+get_set_part2 g1=new get_set_part2();
+g1.set_a(7);
+System.out.println(g1.get_a());
+	}
+
+}
