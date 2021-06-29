c7d47c07d4b7b07c764cdf98895016d8ff04db78
nidhipatel1211 <nidhi121125@gmail.com>
2021-06-29 16:17:28 +0530

    Create inheritance_example.java

---
+++ NidhiPatel/nidhi/src/nidhi/inheritance_example.java
@@ -0,0 +1,16 @@
+package nidhi;
+
+public class inheritance_example {
+
+	public float height;
+	public float weight;
+	public float radius;
+
+	public inheritance_example() {
+		super();
+		height = 0;
+		weight = 0;
+		radius = 0;
+
+	}
+}
