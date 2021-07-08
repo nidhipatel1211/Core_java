52b6312786ee324fd81ed5a8e8488c5474d4c18e
nidhipatel1211 <nidhi121125@gmail.com>
2021-07-08 13:05:32 +0530

    Create polymorphism4.java

---
+++ NidhiPatel/nidhi/src/nidhi/polymorphism4.java
@@ -0,0 +1,10 @@
+package nidhi;
+
+public class polymorphism4 extends polymorphism2{
+
+	public polymorphism4(String name, int i) {
+		super(name, i);
+		// TODO Auto-generated constructor stub
+	}
+
+}
