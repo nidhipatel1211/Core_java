755498f7172542ec3bfb61be743b9e0ab8714216
nidhipatel1211 <nidhi121125@gmail.com>
2021-07-10 16:02:00 +0530

    Create abstract_exe.java

---
+++ NidhiPatel/nidhi/src/nidhi/abstract_exe.java
@@ -0,0 +1,14 @@
+package nidhi;
+
+public class abstract_exe {
+
+	public static void main(String[] args) {
+		abstract_exe1 a1=new abstract_exe2(); 
+		a1.print();
+		a1.add();
+		abstract_exe1 a2=new abstract_exe3();
+		a2.add();
+
+	}
+
+}
