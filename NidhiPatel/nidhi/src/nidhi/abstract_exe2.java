76ab7abf9ca98934f0ffc767ff026b6d8bf15d4b
nidhipatel1211 <nidhi121125@gmail.com>
2021-07-10 16:02:17 +0530

    Create abstract_exe2.java

---
+++ NidhiPatel/nidhi/src/nidhi/abstract_exe2.java
@@ -0,0 +1,12 @@
+package nidhi;
+
+public class abstract_exe2 extends abstract_exe1 {
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		System.out.println(7+8);
+		
+	}
+
+}
