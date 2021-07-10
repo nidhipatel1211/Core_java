168490b7737045ade673bfe28c6e32f5fc3ed1a3
nidhipatel1211 <nidhi121125@gmail.com>
2021-07-10 16:02:08 +0530

    Create abstract_exe1.java

---
+++ NidhiPatel/nidhi/src/nidhi/abstract_exe1.java
@@ -0,0 +1,10 @@
+package nidhi;
+
+public abstract class abstract_exe1 {
+	public void print()
+	{
+		System.out.print("hello this is example of abstract class");
+	}
+	public abstract void add();
+
+}
