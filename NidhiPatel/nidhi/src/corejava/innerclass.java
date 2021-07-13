ca917391e3fa332298fee826616330f9ed5cee0c
nidhipatel1211 <nidhi121125@gmail.com>
2021-07-13 13:20:27 +0530

    Create innerclass.java

---
+++ NidhiPatel/nidhi/src/corejava/innerclass.java
@@ -0,0 +1,15 @@
+package corejava;
+
+
+
+public class innerclass {
+
+	public static void main(String[] args) {
+	innerclsexe i=new innerclsexe();
+System.out.println(i.getName("mango"));
+System.out.println(i.setName("orange"));
+
+
+	}
+
+}
