115fa813b445396932d5f85b95126707db754356
nidhipatel1211 <nidhi121125@gmail.com>
2021-07-23 16:14:40 +0530

    Create static_innercls.java

---
+++ NidhiPatel/nidhi/src/corejava/static_innercls.java
@@ -0,0 +1,11 @@
+package corejava;
+
+public class static_innercls {
+
+	public static void main(String[] args) {
+		static_innercls1.Inner.testingmethod();
+		System.out.println(static_innercls1.Inner.x);
+		
+	}
+
+}
