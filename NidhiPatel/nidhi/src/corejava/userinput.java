32be62166f618b6de6cdae83d8c77e294b46d69c
nidhipatel1211 <nidhi121125@gmail.com>
2021-07-23 15:43:14 +0530

    Create userinput.java

---
+++ NidhiPatel/nidhi/src/corejava/userinput.java
@@ -0,0 +1,15 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class userinput {
+
+	public static void main(String[] args) {
+
+		System.out.println("enter your name");
+		Scanner in=new Scanner(System.in);
+		String name=in.next();
+		System.out.println("welcome "+name);
+	}
+
+}
