fea6b412c69e8f360724736314159c6c7f29e0cd
nidhipatel1211 <nidhi121125@gmail.com>
2021-07-26 12:42:14 +0530

    Create final_class1.java

---
+++ NidhiPatel/nidhi/src/corejava/final_class1.java
@@ -0,0 +1,10 @@
+package corejava;
+
+public final class final_class1 {
+	//we could not able to access or extend data of parent class while declare as final
+	void print()
+	{
+		System.out.println("final class example");
+	}
+
+}
