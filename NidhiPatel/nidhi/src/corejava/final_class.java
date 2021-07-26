76aba211a89317f043543250fdecdcb518db8c13
nidhipatel1211 <nidhi121125@gmail.com>
2021-07-26 12:42:08 +0530

    Create final_class.java

---
+++ NidhiPatel/nidhi/src/corejava/final_class.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class final_class {
+
+	public static void main(String[] args) {
+		final_class1 f1 = new final_class1();
+		final_class2 f2 = new final_class2();
+		f1.print();
+		f2.display();
+
+	}
+
+}
