0dcdab03647eb7e3dcdfd89f6b1baa26b280f066
nidhipatel1211 <nidhi121125@gmail.com>
2021-07-26 12:41:57 +0530

    Create final_method1.java

---
+++ NidhiPatel/nidhi/src/corejava/final_method1.java
@@ -0,0 +1,15 @@
+package corejava;
+
+public class final_method1 {
+	//can not be override in child class using final
+	public final void hello()
+	{
+		System.out.println("hello");
+	}
+	public void hi()
+	{
+		System.out.println("hi");
+	}
+	
+
+}
