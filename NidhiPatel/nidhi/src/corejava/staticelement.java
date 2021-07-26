de2882508a08e1a5f5b28e6febbc6a0cdfe06c36
nidhipatel1211 <nidhi121125@gmail.com>
2021-07-26 12:41:30 +0530

    Update staticelement.java

--- NidhiPatel/nidhi/src/corejava/staticelement.java
+++ NidhiPatel/nidhi/src/corejava/staticelement.java
@@ -1,27 +1,26 @@
 package corejava;
 
 public class staticelement {
-	 static int a=0;
+	static int a = 0;
 
-		public static int getA() {
-			return a;
-		}
+	public static int getA() {
+		return a;
+	}
 
-		public static void count() {
-			a++;
-		}
+	public static void count() {
+		a++;
+	}
 
-		public static void main(String[] args) {
-			staticelement s = new staticelement();
-			System.out.println("current value " + s.getA());
-			s.count();
-			System.out.println("current value " + s.getA());
-			staticelement s1 = new staticelement();
-			s.count();
-			System.out.println("current value " + s1.getA());
-			s.count();
-			System.out.println("current value " + s1.getA());
-		
+	public static void main(String[] args) {
+
+		System.out.println("current value " + staticelement.getA());
+		staticelement.count();
+		System.out.println("current value " + staticelement.getA());
+
+		staticelement.count();
+		System.out.println("current value " + staticelement.getA());
+		staticelement.count();
+		System.out.println("current value " + staticelement.getA());
 
 	}
 
